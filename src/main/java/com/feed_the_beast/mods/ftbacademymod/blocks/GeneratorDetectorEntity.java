927fd12459315c88344adc86dc52aa03a475c11d
LatvianModder <latvianmodder@gmail.com>
2019-06-27 00:10:58 +0300

    Fixed coal generator detector

--- src/main/java/com/feed_the_beast/mods/ftbacademymod/blocks/GeneratorDetectorEntity.java
+++ src/main/java/com/feed_the_beast/mods/ftbacademymod/blocks/GeneratorDetectorEntity.java
@@ -8,16 +8,11 @@ import net.minecraft.util.ResourceLocation;
  */
 public class GeneratorDetectorEntity extends CompletingDetectorEntity
 {
-	public static final ResourceLocation DUCT_ID = new ResourceLocation("actuallyadditions:generator");
+	public static final ResourceLocation DUCT_ID = new ResourceLocation("actuallyadditions:coalgenerator");
 
 	@Override
 	public boolean test(TileEntity tileEntity)
 	{
-		if (DUCT_ID.equals(TileEntity.getKey(tileEntity.getClass())))
-		{
-			return tileEntity.serializeNBT().getInteger("Energy") > 0;
-		}
-
-		return false;
+		return DUCT_ID.equals(TileEntity.getKey(tileEntity.getClass())) && tileEntity.serializeNBT().getInteger("Energy") > 0;
 	}
 }
