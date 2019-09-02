6343d04712b42cb0f2ccc9b633fc2f2f87af8980
LatvianModder <latvianmodder@gmail.com>
2019-09-02 23:08:53 +0300

    Added generator_fuel

--- src/main/java/com/feed_the_beast/mods/ftbacademymod/blocks/EnumDetectorType.java
+++ src/main/java/com/feed_the_beast/mods/ftbacademymod/blocks/EnumDetectorType.java
@@ -23,7 +23,8 @@ public enum EnumDetectorType implements IStringSerializable
 	TANK("tank", 9, TankDetectorEntity.class, TankDetectorEntity::new),
 	CRAFTING_TABLE("crafting_table", 10, CraftingTableDetectorEntity.class, CraftingTableDetectorEntity::new),
 	LUMINOUS_CRAFTING_TABLE("luminous_crafting_table", 11, LuminousCraftingTableDetectorEntity.class, LuminousCraftingTableDetectorEntity::new),
-	ENDO("endo", 12, EndoDetectorEntity.class, EndoDetectorEntity::new);
+	ENDO("endo", 12, EndoDetectorEntity.class, EndoDetectorEntity::new),
+	GENERATOR_FUEL("generator_fuel", 13, GeneratorFuelDetectorEntity.class, GeneratorFuelDetectorEntity::new);
 
 	public static final EnumDetectorType[] VALUES = values();
 	public static final Int2ObjectOpenHashMap<EnumDetectorType> META_MAP = new Int2ObjectOpenHashMap<>();
--- src/main/java/com/feed_the_beast/mods/ftbacademymod/blocks/GeneratorDetectorEntity.java
+++ src/main/java/com/feed_the_beast/mods/ftbacademymod/blocks/GeneratorDetectorEntity.java
@@ -8,11 +8,11 @@ import net.minecraft.util.ResourceLocation;
  */
 public class GeneratorDetectorEntity extends CompletingDetectorEntity
 {
-	public static final ResourceLocation DUCT_ID = new ResourceLocation("actuallyadditions:coalgenerator");
+	public static final ResourceLocation GENERATOR_ID = new ResourceLocation("actuallyadditions:coalgenerator");
 
 	@Override
 	public boolean test(TileEntity tileEntity)
 	{
-		return DUCT_ID.equals(TileEntity.getKey(tileEntity.getClass())) && tileEntity.serializeNBT().getInteger("Energy") > 0;
+		return GENERATOR_ID.equals(TileEntity.getKey(tileEntity.getClass()));
 	}
 }
